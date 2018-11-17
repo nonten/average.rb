5bfca6413d1ff2ea527d3ac7fcf8c341136d9b59
Tennon Barnett <Nonten18@gmail.com>
2018-11-17 06:20:04 -0600

    Start up

---
+++ average.rb
@@ -0,0 +1,10 @@
+a = 12
+b = 7
+c = 5
+d = 10
+
+print "What is the average of 12, 7, 5 and 10? "
+answer = gets
+number = answer.to_f
+correct_answer = (a + b + c + d) / 4.0
+puts "You entered #{number} and the correct answer is #{correct_answer}"
